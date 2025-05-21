77a9dc4ed94d5cd4d54318426be389c1a0f17af2
vdavey <vishvesh.b.davey@gmail.com>
2025-05-21 15:11:22 -0400

    Update footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
             <ul class="icons">
                 <li><a href="https://www.instagram.com/pilotdavey/" class="icon brands alt fa-instagram"><span class="label">Instagram</span></a></li>
                 <li><a href="https://www.linkedin.com/in/vishveshdavey/" class="icon brands alt fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
-                <li><a href="Referrals.html" class="icon brands alt fa-handshake"><span class="label">Affiliate Links</span></a></li>
+                <li><a href="Referrals.html" class="icon alt fa-handshake"><span class="label">Affiliate Links</span></a></li>
             </ul>
             <ul class="copyright">
                 <li>&copy; Pilot Davey</li><li>Design: <a href="https://html5up.net">HTML5 UP</a></li>
